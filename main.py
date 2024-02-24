36e3f462df9bf5d7390a91b1a01e47c86fa9333e
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-02-24 19:35:22 +0530

    feat: create lemmatizer and chunk

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from nltk.tokenize import word_tokenize,sent_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer,WordNetLemmatizer
+import nltk
 import string
 example_string = """Muad'Dib learned rapidly because his first training was in how to learn.
  And the first lesson of all was the basic trust that he could learn.
  It's shocking to find how many people do not believe they can learn,
  and how many more believe learning to be difficult."""
-def remove_punc(input_string):
+def remove_punc(input_string):#remove punctuations
     punctuations=string.punctuation
     output="".join(word for word in example_string if word not in punctuations)
     return output
 removed_punc=remove_punc(example_string)
-words=word_tokenize(removed_punc)
-sent=sent_tokenize(removed_punc)
-stop_words=set(stopwords.words('english'))
-filtered_list=[word for word in sent if word.casefold() not in stop_words]
+words=word_tokenize(removed_punc)#word tokenization
+sentence=sent_tokenize(removed_punc)#sentence tokenization
+stop_words=set(stopwords.words('english'))#checking for stop words
+filtered_list=[word for word in sentence if word.casefold() not in stop_words]
 stemmer=PorterStemmer()
-stemmed_words=[stemmer.stem(word) for word in words]
-print(stemmed_words)
+stemmed_words=[stemmer.stem(word) for word in words]#stemming words
+value=nltk.pos_tag(sentence)#Parts of Speech
+#nltk.help.upenn_tagset()-for getting all the types of sentiments
+lemmatizer=WordNetLemmatizer()#lemmatizing words
+values=[lemmatizer.lemmatize(word) for word in words]
+grammar = "NP: {<DT>?<JJ>*<NN>}"
+chunk_parser=nltk.RegexpParser(grammar)
+tree = chunk_parser.parse(value)
+tree.draw()
